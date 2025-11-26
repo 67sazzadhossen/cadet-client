22a1ce32cc37538b7a685e37118cd283c5043dd7
67sazzadhossen <sazzadhossen010@gmail.com>
2025-11-26 23:59:37 +0600

    c

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -16,6 +16,7 @@ const baseQuery = fetchBaseQuery({
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = (getState() as RootState).auth.token;
+    console.log("token in vercel", token);
     if (token) {
       headers.set("authorization", token);
     }
