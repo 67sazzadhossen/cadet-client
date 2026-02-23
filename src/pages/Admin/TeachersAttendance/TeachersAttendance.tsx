3fa7f5583db65d87a5966723f9e90d76f161f163
67sazzadhossen <sazzadhossen010@gmail.com>
2026-02-23 09:42:22 +0600

    c

--- src/pages/Admin/TeachersAttendance/TeachersAttendance.tsx
+++ src/pages/Admin/TeachersAttendance/TeachersAttendance.tsx
@@ -389,3 +389,8 @@ const TeachersAttendance = () => {
 };
 
 export default TeachersAttendance;
+export async function getServerSideProps() {
+  return {
+    props: {}, // Page will render only on client
+  };
+}
